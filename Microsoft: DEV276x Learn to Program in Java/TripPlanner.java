2b2a3e986a77291954920a0d254cbc9d7a79882b
amitoj-singh <31106921+amitoj-singh@users.noreply.github.com>
2019-08-10 15:47:44 +0530

    Update and rename TripPlanner.java to Microsoft: DEV276x Learn to Program in Java/TripPlanner.java

--- Microsoft: DEV276x Learn to Program in Java/TripPlanner.java
+++ Microsoft: DEV276x Learn to Program in Java/TripPlanner.java
@@ -1,3 +1,9 @@
+/*
+For this project, you are going to write a program that asks the user for some information 
+about an international trip they are taking. Based on that information you will need to do 
+some conversions, using the correct data types, to tell them some information to help them 
+plan their trip. Here is my sample output:
+*/
 import java.util.Scanner;
 public class TripPlanner {
 
